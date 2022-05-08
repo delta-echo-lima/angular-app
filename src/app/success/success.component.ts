50f4f5677d5005b43524ee8547d222466bf29639
DelMouse <del.groh@icloud.com>
2022-05-08 16:38:26 -0500

    add styles

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success',
   templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  //styleUrls: ['./success.component.css']
+  styles:[`
+  p{
+    padding: 20px;
+    background-color: palegreen;
+    border: 1px solid green;
+  }
+  `]
 })
 export class SuccessComponent implements OnInit {
 
